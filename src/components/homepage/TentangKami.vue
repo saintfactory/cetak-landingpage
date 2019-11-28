cb32c55d524305bdd9ff6c496874d46115c47db4
irfan hamid <irfansyukri597@gmail.com>
2019-11-28 12:18:54 +0700

    mobile ui

---
+++ src/components/homepage/TentangKami.vue
@@ -0,0 +1,69 @@
+<template>
+<div class="hero">
+    <div class="row justify-content-md-center">
+        <div class="col-md-5 text-left">
+            <div class="row mb-3">
+                <span class="superFont font-weight-bold mb-2"><i class="fas fa-question mr-2"></i> Tentang Kami</span>
+                <p>Platform percetakan online di Indonesia yang memberikan manfaat serta kenyamanan dalam proses cetak dokumen secara efektif dan efisien.
+                Juga memberikan dampak dalam aspek sosial khususnya dalam pertumbuhan ekonomi.
+                Dalam perjalananya cetakk.id juga terus berusaha untuk meningkatkan performa layanan dari sebuah platform yang menghubungkan 
+                pengguna terhadap mitra-mitra bisnis kami yaitu vendor percetakan dan pengguna.</p>
+            </div>
+
+            <div class="row mb-3">
+                <span class="superFont font-weight-bold mb-2"><i class="fas fa-thumbs-up mr-2"></i> Keuntungan</span>
+                <p>Meminimalisir rusaknya dokumen dan infeksi virus, efisiensi dalam proses cetak dokumen,
+                dan turut serta meningkatkan ekonomi Indonesia di bidang percetakan
+                khususnya untuk individu yang mempunyai mesin printer.</p>
+            </div>
+
+            <div class="row mb-3 mt-5">
+                <a href="https://www.instagram.com/cetakkid.official/">
+                    <img class="img-tumbs" src="../../assets/img/instagram.png" alt="">
+                </a>
+                <a href="https://www.linkedin.com/company/cetakk-id/">
+                    <img class="img-tumbs" src="../../assets/img/linkedin.png" alt="">
+                </a>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <img src="../../assets/home-paid.png" alt="">
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.hero{
+    /* background:url('../../assets/img/vectorBg.png') no-repeat center;
+    background-size: cover; */
+    min-height: 100vh;
+	background-color: rgb(255, 255, 255);
+    padding-top: 150px;
+    /* background-position-y: 70px      */
+}
+
+.superFont{
+    font-size: 35px
+}
+
+p{
+    font-size: 13.5px
+}
+
+.img-tumbs{
+    min-width: 48px;
+    max-width: 48px;
+    margin-right: 10px
+}
+
+.img-tumbs:hover{
+    transition: transform .5s !important;
+	transform: scale(1.1) !important
+}
+
+</style>
--- src/components/layouter/Based.vue
+++ src/components/layouter/Based.vue
@@ -3,8 +3,9 @@
         <div class="header">
             <nav class="navbar navbar-expand-lg navbar-dark nav-content fixed-top">
             <div class="container-fluid">
-                <a class="navbar-brand"><router-link to="/home" class="nav-link">
-                    <img src="../../assets/cetakkPutih.png" height="40" alt="">
+                <a class="navbar-brand">
+                    <router-link to="/home" class="nav-link">
+                        <img src="../../assets/cetakkPutih.png" height="40" alt="">
                     </router-link>
                     </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,10 +14,10 @@
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a><router-link to="/cara" class="nav-link text-light">Cara Cetak</router-link></a>
+                        <a><router-link to="/cara" class="nav-link menus-expand text-light">Cara Penggunaan</router-link></a>
                     </li>
                     <li class="nav-item">
-                        <a><router-link to="/tentang" class="nav-link text-light">Tentang Kami</router-link></a>
+                        <a><router-link to="/tentang" class="nav-link menus-expand text-light">Tentang</router-link></a>
                     </li>
                 </ul>
 
@@ -45,7 +46,37 @@ import '../../firebase'
 export default {
     name: 'Based',
 }
-
 </script>
+
+<style scoped>
+.nav-content{
+    background: #0D7BE5;
+    z-index: 200;
+    height: 70px;
+}
+li{
+    margin: 10px;
+}
+
+.nav-item{
+    font-weight: 600 !important;
+}
+
+.nav-item:hover, .nav-link:hover{
+    transition: transform .2s !important;
+    transform: scale(1.01) !important;
+    font-weight: 700 !important;
+}
+
+@media (max-width: 790px){
+	.navbar-collapse{
+        background-color: rgba(23, 23, 23, 0.898);
+        border-radius: 10px
+    }
+    .menus-expand{
+        background-color: transparent;
+    }
+}
+</style>
 
 
